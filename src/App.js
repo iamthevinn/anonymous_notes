3e8345183acc22df0e9fab0238427ff557182307
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-13 11:02:29 -0600

    styled and profiled

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './ui-toolkit/css/nm-cx/main.css'
+
+const InputNote = (props) => {
+  return (
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} className="card">
+      <div className="inputContent inputLabel">
+        Note:
+      </div>
+      <div className="inputContent inputTextBox">
+        <input type="text"></input>
+      </div>
+      <div className="inputContent inputButtonContainer">
+        <button className="inputButton">Add Note</button>
+      </div>
+    </div>
+  )
+}
+
+const Note = (props) => {
+  return (
+    <div className="card noteCard">
+      Words
+    </div>
+  )
+}
+
+const ScrollBox = (props) => {
+  return (
+    <div className="card scrollBox">
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="card outerCard">
+          <div className="header">
+            <h1>Anonymous Notes</h1>
+          </div>
+          <InputNote />
+          <ScrollBox />
+        </div>
       </div>
     );
   }
